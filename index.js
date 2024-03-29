aa6aa7b9748be238a76d205758728077ac7bf275
Fatuma Ibrahim <fatmazahraabdulrashid@gmail.com>
2024-03-29 22:32:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    const hq = 42;
+    let distance;
+    
+    if (blocks >= hq) {
+        distance = blocks - hq;
+    } else {
+        distance = hq - blocks;
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(feet) {
+    const hq = 42;
+    let distance;
+
+    if (feet >= hq) {
+        distance = (feet - hq) * 264;
+    } else {
+        distance = (hq - feet) * 264;
+    }
+    return distance;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distance;
+
+    if (start <= destination) {
+        distance = (destination - start) * 264;
+    } else {
+        distance = (start - destination) * 264;
+    }
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    }else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    }else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+}
+    
+        
+
